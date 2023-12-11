fa874a23a30fad0074e3eb4c839789d0d75eae22
Audrey <audrey.galacho@gmail.com>
2023-12-11 17:19:54 +0100

    Feat(config) add logger system

--- src/main/java/fr/eni/filmotheque/controllers/FilmController.java
+++ src/main/java/fr/eni/filmotheque/controllers/FilmController.java
@@ -5,6 +5,9 @@ import fr.eni.filmotheque.bll.GenreService;
 import fr.eni.filmotheque.bo.Film;
 
 import jakarta.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -21,15 +24,25 @@ import java.util.stream.IntStream;
 @SessionAttributes("genres")
 public class FilmController {
 
+    // Déclaration du logger
+    Logger logger = LoggerFactory.getLogger(FilmController.class);
+
+
+    // Déclaration des services utilisés
     public FilmService filmService;
     public GenreService genreService;
 
+    // Constructeur du FilmController et injection des services
     @Autowired
     public FilmController(FilmService filmService, GenreService genreService) {
         this.filmService = filmService;
         this.genreService = genreService;
     }
 
+    /**
+     * Affiche une liste d'années pour la saisie d'un nouveau film
+     * @return
+     */
     @ModelAttribute("years")
     public List<Integer> populateYears() {
         int currentYear = LocalDate.now().getYear();
@@ -59,27 +72,26 @@ public class FilmController {
 
         //mapping avec la vue
         model.addAttribute("films", films);
+        logger.info("Demande de la liste des films");
 
         // affichage de la vue concernée
         return "films";
     }
 
-    //@RequestMapping(path = "/films/detail{id}", method = RequestMethod.GET)
-    //public String displayFilmDetail(Model model, @PathVariable(name= "id") int idFilm) {
 
     /**
-     * Methode qui le detail d'un film en fonction de son id
-     *
+     * Methode qui affiche le detail d'un film en fonction de son id
      * @param model
      * @param idFilm
      * @return
      */
     @RequestMapping(path = "/films/detail", method = RequestMethod.GET)
     public String displayFilmDetail(Model model, @RequestParam(name = "id") int idFilm) {  //injection de Model
-
+        logger.info("Demande d'affichage d'un film d'id :" + idFilm);
         Optional<Film> filmOptional = filmService.findFilmById(idFilm);
         if (filmOptional.isEmpty()) {
             // affichage de la vue concernée
+            logger.error("PAS de film à l'id demander " +idFilm);
             return "error_sql";
         } else {
             Film film = filmOptional.get();
@@ -91,39 +103,30 @@ public class FilmController {
     }
 
     //******************* GESTION DU FORMULAIRE ***************************************
-
     /**
-     * Methode d'affichage du formulaire d'un film
+     * Route d'affichage du formulaire d'un film
      * @param model
      * @return
      */
     @GetMapping(path = "/ajoutfilm")
     public String addFilm(Model model) {
-//        if(Binding.hasError){
-//
-//        }
         model.addAttribute("film", new Film());
-
         return "filmFormAdd";
     }
 
-    //    SUBMIT FORM
 
     /**
-     *
      * @param film
      * @return
      */
     @PostMapping(path = {"/ajoutfilm"})
     public String addFilm(@Valid @ModelAttribute("film") Film film, BindingResult validFilm){//Gestion de la validation des attributs
         if(validFilm.hasErrors()){
-            System.out.println("ERREUR");
+            logger.warn("Erreur dans la validation des données entrées par l'utilisateur");
             return "filmFormAdd";
         }
-
         filmService.addFilm(film);
-
-        System.out.println(film);
+        logger.info("Film correctement inséré");
         return "redirect:/films";
     }
 }
