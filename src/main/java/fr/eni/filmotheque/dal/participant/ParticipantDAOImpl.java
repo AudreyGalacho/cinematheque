848d8591cc29aeb1a8553554846ac8c45284379c
Audrey <audrey.galacho@gmail.com>
2023-12-11 17:32:47 +0100

    éAdd comment sand documentation methodÃ

--- src/main/java/fr/eni/filmotheque/dal/participant/ParticipantDAOImpl.java
+++ src/main/java/fr/eni/filmotheque/dal/participant/ParticipantDAOImpl.java
@@ -15,10 +15,11 @@ import java.util.*;
 @Repository
 public class ParticipantDAOImpl implements ParticipantDAO{
 
+    // LOGGER trace use
     Logger logger = LoggerFactory.getLogger(ParticipantDAOImpl.class);
 
 
-    //    JDBC CONNEXION *************************************************
+    // JDBC CONNEXION *************************************************
     private JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -26,6 +27,15 @@ public class ParticipantDAOImpl implements ParticipantDAO{
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
+    // Methodes implementee *********************************************
+
+    /**
+     * Methode de récupération de la liste des acteurs par l'id du film
+     * @param idFilm
+     * @return
+     */
+    // TODO retourne un optionnal (tout les film n'ont pas des acteurs
     @Override
     public List<Participant> getListActeursByFilm(Integer idFilm) {
         List<Participant> listeActeurs= new ArrayList<>();
@@ -44,10 +54,13 @@ public class ParticipantDAOImpl implements ParticipantDAO{
         return listeActeurs;
     }
 
+    /**
+     * Méthode de récupérationde la liste des participants d'un film(Réalisateur ou Acteurs)
+     * @return
+     */
     public List<Participant> getParticipantsList() {
         List<Participant> listeParticipant = new ArrayList<>();
 
-        // requete
         String sql = "SELECT id, prenom , nom FROM participants ";
         try {
             listeParticipant = jdbcTemplate.query(sql, new BeanPropertyRowMapper (Participant.class));
@@ -57,24 +70,34 @@ public class ParticipantDAOImpl implements ParticipantDAO{
         }
         return listeParticipant;
     }
+
+    /**
+     * Méthode de récupération des information d'un participant par son id (Optional)
+     * @param id
+     * @return
+     */
     public Participant getParticipantById(Integer id){
-        Optional<Participant> participantById = Optional.of(new Participant());
+        Optional<Participant> participantByIdOpt = Optional.of(new Participant());
 
         String sql = "SELECT id, prenom, nom FROM participants WHERE id = ? ";
 
-        /* Utilisation d'un RowMapper pour faire correspondre le résultat de la requête avec un objet */
         try {
-            participantById = (Optional.ofNullable(jdbcTemplate.queryForObject(sql,
+            participantByIdOpt = (Optional.ofNullable(jdbcTemplate.queryForObject(sql,
                     (ResultSet rs, int rowNum) -> new Participant(
                             rs.getInt(1),
                             rs.getString(2),
                             rs.getString(3)),
                     id)));
+            // Si récup ok
+            Participant participantById = participantByIdOpt.get();
+            return participantById;
+
         } catch (Exception e) {
             logger.warn("Erreur dal getParticipantById() sql error");
             e.printStackTrace();
         }
 
-        return participantById.get();
+        // TODO faire remonter l'optional (retourner un Optional<Participant>)
+        return participantByIdOpt.get();
     }
 }
