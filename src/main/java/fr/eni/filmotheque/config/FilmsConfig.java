b1cd09c4057c76934a2e21b7d499f13cbc830905
Audrey <audrey.galacho@gmail.com>
2023-12-13 18:03:42 +0100

    Refactor

--- src/main/java/fr/eni/filmotheque/config/FilmsConfig.java
+++ src/main/java/fr/eni/filmotheque/config/FilmsConfig.java
@@ -1,12 +1,6 @@
 package fr.eni.filmotheque.config;
 
-import fr.eni.filmotheque.bll.GenreService;
-import fr.eni.filmotheque.bo.Genre;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.ApplicationScope;
-
-import java.util.List;
 
 @Configuration
 public class FilmsConfig {
